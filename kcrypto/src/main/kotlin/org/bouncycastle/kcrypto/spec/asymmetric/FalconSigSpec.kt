8dc6acdee8ebdd91f8774187a3e06c256bd4a1ba
David Hook <dgh@cryptoworkshop.com>
2023-12-07 14:18:55 +1100

    corrected falcon OID.

--- kcrypto/src/main/kotlin/org/bouncycastle/kcrypto/spec/asymmetric/FalconSigSpec.kt
+++ kcrypto/src/main/kotlin/org/bouncycastle/kcrypto/spec/asymmetric/FalconSigSpec.kt
@@ -1,6 +1,5 @@
 package org.bouncycastle.kcrypto.spec.asymmetric
 
-import org.bouncycastle.asn1.ASN1ObjectIdentifier
 import org.bouncycastle.asn1.bc.BCObjectIdentifiers
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier
 import org.bouncycastle.kcrypto.BaseSigningKey
@@ -12,8 +11,8 @@ import org.bouncycastle.pqc.jcajce.interfaces.FalconKey
 import org.bouncycastle.pqc.jcajce.spec.FalconParameterSpec
 
 // these are only in 1.0.2
-private val falcon_512 = ASN1ObjectIdentifier("1.3.9999.3.1") // falcon.branch("1");
-private val falcon_1024 = ASN1ObjectIdentifier("1.3.9999.3.4") // falcon.branch("2");
+private val falcon_512 = BCObjectIdentifiers.falcon_512 // falcon.branch("1");
+private val falcon_1024 = BCObjectIdentifiers.falcon_1024 // falcon.branch("2");
 
 
 class FalconSigSpec: SigAlgSpec {
